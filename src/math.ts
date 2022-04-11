dd00605f0eed8d79e6b77884605f3755acc74d71
rubybb <10256477+rubybb@users.noreply.github.com>
2022-04-10 20:24:04 -0600

    fix(math): :label: explictly declare clamp return type

--- src/math.ts
+++ src/math.ts
@@ -1,11 +1,11 @@
 /**
  * Clamp value to be within the minimum and maximum range.
  * This will round to the nearest boundary if the value is outside of it.
- * 
+ *
  * @param value The original value.
  * @param min The minimum possible value.
  * @param max The maximum possible value.
  */
-export function clamp (value: number, min: number, max: number) {
+export function clamp (value: number, min: number, max: number): number {
 	return (value > max ? max : (value < min ? min : value));
 }
