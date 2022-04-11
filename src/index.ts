a803a808afd8d85f787169db9348f7d6009ee003
rubybb <10256477+rubybb@users.noreply.github.com>
2022-04-10 20:26:25 -0600

    feat(objects): :sparkles: add flatten and unflatten

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-unresolved */
 
 export * as functions from "./functions";
 export * as math from "./math";
 export * as objects from "./objects";
 export * as random from "./random";
+
+export type Expand <T> = T extends T ? { [P in keyof T]: T[P] } : never;
+export type UnionToIntersection <U> = (U extends U ? ((x: U) => any) : never) extends (x: infer I) => any ? I : never;
--- src/objects.ts
+++ src/objects.ts
@@ -1,11 +1,46 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import type { Expand, UnionToIntersection } from ".";
 
 export type Keys <T> = keyof T;
 export type Merge <A, B> = {[K in Keys<A> | Keys<B>]: A[K & Keys<A>] | B[K & Keys<B>]};
 export type Cast<X, Y> = X extends Y ? X : Y;
 
-export type Constructor = new (...args: any) => any;
-export type AbstractConstructor = abstract new (...args: any) => any;
+export type Flatten <T, Z extends string = ""> = T extends object
+	? Expand<UnionToIntersection<{
+		[P in keyof T]: P extends string | number
+			? T[P] extends object
+				? Flatten<T[P], `${Z}${P}.`>
+				: { [Q in `${Z}${P}`]: T[P] }
+			: never
+	}[keyof T & (T extends readonly unknown[] ? number : unknown)]>>
+	: T;
+
+type PrefixKeyof<T extends { [key: string]: unknown }> = {
+	[P in keyof T]: P extends `${infer Q}.${string}` ? Q : never;
+}[keyof T];
+
+export type Unflatten <T extends { [key: string]: unknown }> = Expand<UnionToIntersection<
+	| UnflattenDirect<T>
+	| UnflattenIndirect<T, PrefixKeyof<T>>
+>>;
+
+type UnflattenDirect <T extends { [key: string]: unknown }> = {
+	[P in keyof T]: P extends `${string}.${string}`
+		? never
+		: { [Q in P]: T[P] }
+}[keyof T];
+
+type UnflattenIndirect <T extends { [key: string]: unknown }, Z extends string> = Z extends Z
+	? {
+		[P in Z]: Expand<UnionToIntersection<{
+			[P in keyof T]: P extends `${Z}.${infer Affix}`
+				? Unflatten<{ [Q in Affix]: T[P] }>
+				: never
+		}[keyof T]>>
+	}
+	: never;
 
 /** Array<[key, value]> */
 export type Entries <T> = Array<{ [K in Keys<T>]: [K, T[K]] }[Keys<T>]>;
@@ -23,9 +58,9 @@ export type MapCallback <T> = (key: Keys<T>, value: T[typeof key], object: T) =>
  * Returns the names of the enumerable string properties and methods of an object.
  * @param object The original object.
  */
-export const keys = <T> (object: T): Array<Keys<T>> => 
+export const keys = <T> (object: T): Array<Keys<T>> =>
 	Object.keys(object) as Array<Keys<T>>;
-	
+
 /**
  * Returns an array of key/values of the enumerable properties of an object.
  * @param object The original object.
@@ -53,9 +88,9 @@ export function create <T> (object?: Readonly<T>): T {
 /**
  * Remove keys from an object based on a function
  * predicate without mutating the original.
- * 
+ *
  * @param object The original object.
- * @param predicate A predicate function to determine which keys to retain. 
+ * @param predicate A predicate function to determine which keys to retain.
  * The filter method calls the predicate function one time for each element in the array.
  */
 export function filter <T, R> (object: Readonly<T>, predicate: FilterPredicate<T>): R {
@@ -64,7 +99,7 @@ export function filter <T, R> (object: Readonly<T>, predicate: FilterPredicate<T
 
 /**
  * Transform an object without mutating the original.
- * 
+ *
  * @param object The original object.
  * @param callback The map method calls the callback function one time for each element in the array.
  */
@@ -73,9 +108,9 @@ export function map <T, R> (object: Readonly<T>, callback: MapCallback<T>): R {
 }
 
 /**
- * Pick keys from an object. This function creates a new object 
+ * Pick keys from an object. This function creates a new object
  * with the specified keys. The original object is not mutated.
- * 
+ *
  * @param object The original object.
  * @param keys An array of keys to keep.
  */
@@ -87,15 +122,71 @@ export function pick <T, K extends Array<Keys<T>>> (object: Readonly<T>, keys: K
 }
 
 /**
- * Remove keys from an object. This function creates a new object 
+ * Remove keys from an object. This function creates a new object
  * without the specified keys. The original object is not mutated.
- * 
+ *
  * @param object The original object.
  * @param keys An array of keys to remove.
  */
 export function omit <T, K extends Array<Keys<T>>> (object: Readonly<T>, keys: K): Omit<T, K[number]> {
 	const newObject = create(object);
 	for (const key of keys) delete newObject[key];
+
+	return newObject;
+}
+
+/**
+ * Turn an object into a single depth object.
+ * @param object The original object.
+ */
+export function flatten <T> (object: T): Flatten<T> {
+	const newObject = create<any>();
+
+	function step (stepObject: any, parentKey?: string | number, depth: number = 0): void {
+		for (const [key, value] of entries(stepObject)) {
+			if (typeof key === "symbol") continue; // ignore symbols.
+			const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+			if (typeof value === "object") {
+				return void step(value, newKey, depth + 1);
+			}
+
+			newObject[newKey] = value;
+		}
+	}
+
+	step(object);
+	return newObject;
+}
+
+/**
+ * Revert a flattened object to a nested object.
+ * @todo This currently doesn't deal with arrays.
+ * @param object The flattened object.
+ */
+export function unflatten <T extends { [key: string]: unknown; }> (object: T): Unflatten<T> {
+	const newObject = create<any>();
+
+	for (const key of keys(object)) {
+		if (typeof key !== "string") continue;
+
+		const nestedKeys = key.split(".").reverse();
+		let receipt = newObject;
+
+		while (nestedKeys.length >= 1) {
+			const nestedKey = nestedKeys.pop();
+			if (!nestedKey) continue;
+
+			receipt[nestedKey] ??= {};
+
+			if (nestedKeys.length === 0) {
+				receipt[nestedKey] = object[key];
+				continue;
+			}
+
+			receipt = receipt[nestedKey];
+		}
+	}
 
 	return newObject;
 }
